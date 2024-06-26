54f91fd020adc13875e2591cede5634e54d50c01
ShyShouko <denis1195@gmail.com>
2024-06-26 19:37:06 +0300

    worked version

--- src/main/java/org/example/config/CassandraConfig.java
+++ src/main/java/org/example/config/CassandraConfig.java
@@ -1,30 +1,32 @@
 package org.example.config;
 
+import com.datastax.oss.driver.api.core.CqlSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
 import org.springframework.data.cassandra.core.mapping.BasicCassandraMappingContext;
 import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
 
 @Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
+
     @Override
-
     protected String getKeyspaceName() {
-        return "testKeySpace";
-    }
-    @Bean
-    public CassandraClusterFactoryBean cluster() {
-        CassandraClusterFactoryBean cluster =
-                new CassandraClusterFactoryBean();
-        cluster.setContactPoints("127.0.0.1");
-        cluster.setPort(9142);
-        return cluster;
+        return "testKeySpace"; // Укажите имя вашего keyspace
     }
 
     @Bean
-    public CassandraMappingContext cassandraMapping()
-            throws ClassNotFoundException {
+    public CqlSessionFactoryBean session() {
+        CqlSessionFactoryBean session = new CqlSessionFactoryBean();
+        session.setContactPoints("127.0.0.1"); // Укажите адрес вашего Cassandra сервера
+        session.setPort(9142); // Укажите порт вашего Cassandra сервера
+        session.setKeyspaceName(getKeyspaceName());
+        return session;
+    }
+
+    @Bean
+    public CassandraMappingContext cassandraMapping() throws ClassNotFoundException {
         return new BasicCassandraMappingContext();
     }
 }
