e26f0e667fcda26e67614e35dff7089615bce3d2
A1exDUCK <leha.suchkov.2014@mail.ru>
2024-07-02 15:13:07 +0300

    Update model,add generate id

--- src/main/java/org/example/model/Timetable.java
+++ src/main/java/org/example/model/Timetable.java
@@ -3,7 +3,6 @@ package org.example.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
@@ -14,10 +13,19 @@ import java.util.UUID;
 @AllArgsConstructor
 @Data
 public class Timetable {
+
     @PrimaryKey
     private UUID id;
     private UUID userId;
     private String lectorName;
     private String disciplineName;
     private Integer classroom;
+
+    public Timetable(String lectorName, String disciplineName, Integer classroom) {
+        this.id = UUID.randomUUID();
+        this.userId = UUID.randomUUID();
+        this.lectorName = lectorName;
+        this.disciplineName = disciplineName;
+        this.classroom = classroom;
+    }
 }
